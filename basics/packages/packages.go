c091914b0313335dd8f9b92c5424a5b0405895c1
averted <vitiuk.alex@gmail.com>
2016-05-14 14:16:25 -0400

    Basics: packages

---
+++ basics/packages/packages.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println("My favourite number is:", rand.Intn(10))
+	fmt.Println(math.Pi)
+}
